4378d669e9eba68e3612cc5efa1996e041216a65
chororinko <147678517+chororinko@users.noreply.github.com>
2023-12-17 09:28:16 +0000

    [Finish]add genre

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -1,16 +1,17 @@
 class Admin::GenresController < ApplicationController
   before_action :authenticate_admin!
-  
+
   def create
     @genre = Genre.new(genre_params)
     if @genre.save
-    flash[:notice] = "ジャンル登録に成功しました"
+    flash[:notice] = "新規登録に成功しました"
       redirect_to request.referer
     else
       @genres = Genre.all
       redirect_to request.referer
+    end
   end
-  
+
   def index
     @genres = Genre.all
     @genre = Genre.new
@@ -19,19 +20,19 @@ class Admin::GenresController < ApplicationController
   def edit
     @genre = Genre.find(params[:id])
   end
-  
+
   def update
     @genre = Genre.find(params[:id])
     if @genre.update(genre_params)
       flash[:notice] = '変更を保存しました'
-      redirect_to admin_genre_path(@genre.id)
+      redirect_to admin_genres_path
     else
       render :edit
     end
   end
-  
+
   private
-  
+
   def genre_params
     params.require(:genre).permit(:name)
   end
