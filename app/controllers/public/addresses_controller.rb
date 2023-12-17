6f9cd46bb145d0772f4a3d94f89df7b1c23efdd1
vermouthA28 <147418988+vermouthA28@users.noreply.github.com>
2023-12-17 07:26:38 +0000

    [Update]

--- app/controllers/public/addresses_controller.rb
+++ app/controllers/public/addresses_controller.rb
@@ -5,6 +5,18 @@ class Public::AddressesController < ApplicationController
     @address = Address.new
   end
 
+  def create
+    @address = Address.new(address_params)
+    @address.user_id = customer_id
+    @address.save
+    redirect_to request.referer
+  end
+
   def edit
+  end
+
+  private
+  def address_params
+    params.require(:address).permit(:post_code, :address, :name)
   end
 end
