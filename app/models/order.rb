9b026cc1e2464194a6beb1cc56817dfb3cf61356
vermouthA28 <147418988+vermouthA28@users.noreply.github.com>
2023-12-18 09:05:26 +0000

    [Update]enum

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,2 +1,4 @@
 class Order < ApplicationRecord
+  enum payment_method: { credit_card: 0, transfer: 1 }
+  enum status: { waiting_for_payment: 0, payment_confirmation: 1, production: 2, preparation_for_shipping: 3, shipped: 4 }
 end
