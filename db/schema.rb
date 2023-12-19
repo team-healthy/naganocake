70d4afab4589be36c1d1e1c4df9573925801cecf
vermouthA28 <147418988+vermouthA28@users.noreply.github.com>
2023-12-19 10:34:04 +0000

    [Update]db

--- db/schema.rb
+++ db/schema.rb
@@ -116,14 +116,14 @@ ActiveRecord::Schema.define(version: 2023_12_16_065128) do
   end
 
   create_table "orders", force: :cascade do |t|
-    t.integer "customer_id"
-    t.string "post_code"
-    t.string "adress"
-    t.string "name"
-    t.integer "shipping_cost"
-    t.integer "total_payment"
-    t.integer "payment_method"
-    t.integer "status"
+    t.integer "customer_id", null: false
+    t.string "post_code", null: false
+    t.string "adress", null: false
+    t.string "name", null: false
+    t.integer "shipping_cost", null: false
+    t.integer "total_payment", null: false
+    t.integer "payment_method", null: false
+    t.integer "status", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
