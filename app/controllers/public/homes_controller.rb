c80d12cb110a09f851e8adb52859fd088bedaeab
TomokaAizen <147827756+aizentomoka@users.noreply.github.com>
2023-12-20 04:51:43 +0000

    [update]layout

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -1,7 +1,16 @@
 class Public::HomesController < ApplicationController
   def top
+    @new_items = Item.order(created_at: :desc).limit(4)
   end
 
   def about
   end
+end
+
+
+private
+
+
+def item_params
+  params.require(:item).permit(:name, :non_taxed_price)
 end
