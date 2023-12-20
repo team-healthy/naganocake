0233ea444ec04742b622f0b18418e55bc8d14254
chororinko <147678517+chororinko@users.noreply.github.com>
2023-12-20 10:52:07 +0000

    [Update]cart_items/index,show

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,54 +1,50 @@
 class Public::CartItemsController < ApplicationController
   before_action :authenticate_customer!
+  # before_action :cart_item_item?, only: [:create]
 
   def index
+  	@cart_item = CartItem.new
     @cart_items = current_customer.cart_items
   end
 
   def create
-	if current_customer.cart_items.count >= 1 #カート内に商品があるか？
-	  if nil != current_customer.cart_items.find_by(item_id: params[:cart_item][:item_id]) #カートに入れた商品はすでにカートに追加済か？
-		   @cart_item_u = current_customer.cart_items.find_by(item_id: params[:cart_item][:item_id]) #カート内のすでにある商品の情報取得
-		   @cart_item_u.amount += params[:cart_item][:number_of_items].to_i #既にある情報に個数を合算
-		   @cart_item_u.update(amount: @cart_item_u.amount) #情報の更新　個数カラムのみ
-		   redirect_to cart_items_path #カートページ遷移
-		else
-			 	@cart_item = CartItem.new(cart_item_params) #新しくカートの作成
-			  @cart_item.customer_id = current_customer.id #誰のカートか紐付け
-		  if @cart_item.save #情報を保存できるか？
-				 redirect_to cart_items_path #カートページ遷移
-		  else
-				@items = Item.where(is_active: 0) #販売ステータスが「0」のものを見つける
-		    @quantity = Item.count #商品の数をカウント
-		     #有効、無効ステータスが0のものを見つける
-				render 'index' #indexアクションを呼び出す
-		  end
-	  end
-
-	else
-		@cart_item = CartItem.new(cart_item_params)#新しくカートの作成
-		@cart_item.customer_id = current_customer.id#誰のカートか紐付け
-		if @cart_item.save#情報を保存できるか？
-			 redirect_to cart_items_path#カートページ遷移
-		else
-			@items = Item.where(is_active: 0)#販売ステータスが「0」のものを見つける
-	    @quantity = Item.count#商品の数をカウント
-	    #有効、無効ステータスが0のものを見つける
-			render 'index'#indexアクションを呼び出す
-		end
-	end
+  	@cart_item = CartItem.new(cart_item_params)
+  	#カートにすでに商品が入っていた場合
+  	if @cart_item_exist = CartItem.find_by(item_id: @cart_item.item_id, customer_id: current_customer)
+  	  @cart_item_exist.amount = @cart_item_exist.amount + @cart_item.amount
+  	  @cart_item_exist.update(amount: @cart_item_exist.amount)
+    else
+    	# カートに何も入っていない場合
+    	@cart_item.customer_id = current_customer.id
+    	@cart_item.save
+  	end
+  	redirect_to cart_items_path
   end
 
   def update
+  	cart_item = CartItem.find(params[:id])
+  	cart_item.update(cart_item_params)
+  	redirect_to cart_items_path
   end
 
   def destroy
+  	cart_item = CartItem.find(params[:id])
+  	cart_item.destroy
+  	redirect_to cart_items_path
+  end
+
+  def destroy_all
+
   end
 
   private
 
+ # def cart_item_item?
+	# 	redirect_to item_path(params[:cart_item, :item_id]), notice: "個数を入力してください。" if params[:cart_item, :amount].empty?
+	# end
+
   def cart_item_params
     params.require(:cart_item).permit(:item_id, :amount, :customer_id)
   end
-
 end
+
