a5795bd6559ce924e08b0488acf8c9ca4782c0bf
chororinko <147678517+chororinko@users.noreply.github.com>
2023-12-21 09:34:11 +0000

    [Update]cart_items/index,show

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,18 +4,26 @@ class Item < ApplicationRecord
   belongs_to :genre
   has_many :cart_items, dependent: :destroy
 
-  validates :image, presence: true
+
   validates :name, presence: true
   validates :introduction, presence: true
   validates :genre_id, presence: true
   validates :price, presence: true
-  validates :is_active, presence: true
+  # validates :is_active, presence: true
 
   enum is_active: {販売中:0, 販売停止中:1}
 
   # 消費税を求めるメソッド
   def with_tax_price
     (price * 1.1).floor
+  end
+
+  def get_image(width, height)
+    unless image.attached?
+      file_path = Rails.root.join('app/assets/images/noimage.jpg')
+      image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+    end
+      image.variant(resize_to_limit: [width, height]).processed
   end
 
 end
