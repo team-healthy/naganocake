e24c39ef0329fb08826a5e11a9089a22858a103f
chororinko <147678517+chororinko@users.noreply.github.com>
2023-12-21 10:06:54 +0000

    [Update]cart_items/index,show

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,6 +3,7 @@ class Item < ApplicationRecord
   has_one_attached :image
   belongs_to :genre
   has_many :cart_items, dependent: :destroy
+  has_many :order_details, dependent: :destroy
 
 
   validates :name, presence: true
