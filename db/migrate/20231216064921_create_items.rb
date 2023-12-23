4303983e0631768f66d552879c7aa9311026aa40
vermouthA28 <147418988+vermouthA28@users.noreply.github.com>
2023-12-23 10:28:08 +0000

    [Update]complete

--- db/migrate/20231216064921_create_items.rb
+++ db/migrate/20231216064921_create_items.rb
@@ -1,10 +1,10 @@
 class CreateItems < ActiveRecord::Migration[6.1]
   def change
     create_table :items do |t|
-      t.integer :genre_id
-      t.string :name
-      t.text :introduction
-      t.integer :price
+      t.integer :genre_id, null: false
+      t.string :name, null: false
+      t.text :introduction, null: false
+      t.integer :price, null: false
       t.boolean :is_active, default: true, null: false
  
       t.timestamps
